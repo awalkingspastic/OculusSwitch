0d61627392fed55543ba12254f97082fcfdb483b
Ryan <ryan.camacho92@yahoo.com>
2016-09-10 19:08:33 -0700

    Source Code
    Source Code added

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace OculusOnOffForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.turnOnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turnOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Oculus Switch";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.turnOnToolStripMenuItem,
+            this.turnOffToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 114);
+            // 
+            // turnOnToolStripMenuItem
+            // 
+            this.turnOnToolStripMenuItem.Name = "turnOnToolStripMenuItem";
+            this.turnOnToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.turnOnToolStripMenuItem.Text = "Turn On";
+            this.turnOnToolStripMenuItem.ToolTipText = "Turn On the Oculus";
+            this.turnOnToolStripMenuItem.Click += new System.EventHandler(this.turnOnToolStripMenuItem_Click);
+            // 
+            // turnOffToolStripMenuItem
+            // 
+            this.turnOffToolStripMenuItem.Name = "turnOffToolStripMenuItem";
+            this.turnOffToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.turnOffToolStripMenuItem.Text = "Turn Off";
+            this.turnOffToolStripMenuItem.ToolTipText = "Turn Off the Oculus";
+            this.turnOffToolStripMenuItem.Click += new System.EventHandler(this.turnOffToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 147);
+            this.Name = "Form1";
+            this.Text = "Oculus Switch";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem turnOnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turnOffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OculusOnOffForms
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon notifyIcon;
+
+        public Form1()
+        {
+            InitializeComponent();    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            Hide();
+
+        }
+
+        //This Starts the Oculus
+        private void turnOnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Process.Start(@"C:\StopOculus.bat");
+            ProcessStartInfo info = new ProcessStartInfo(@"C:\StartOculus.bat");
+            info.CreateNoWindow = true;
+            info.UseShellExecute = false;
+            Process processChild = Process.Start(info);
+
+            {
+                    this.notifyIcon1.BalloonTipText = "Oculus has been turned On";
+                    this.notifyIcon1.BalloonTipTitle = "Oculus Switch";
+                    this.notifyIcon1.Icon = new Icon(@"C:\Oculus.ico");
+                    this.notifyIcon1.Visible = true;
+                    this.notifyIcon1.ShowBalloonTip(5);
+                    this.notifyIcon1.Icon = new Icon(@"C:\OculusGreen2.ico");
+                }
+
+        }
+
+        //This Stops the Oculus
+        private void turnOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Process.Start(@"C:\StopOculus.bat");
+            // MessageBox.Show("Oculus has been turned Off");
+            ProcessStartInfo info = new ProcessStartInfo(@"C:\StopOculus.bat");
+            info.CreateNoWindow = true;
+            info.UseShellExecute = false;
+            Process processChild = Process.Start(info);
+            {
+                    this.notifyIcon1.BalloonTipText = "Oculus has been turned Off";
+                    this.notifyIcon1.BalloonTipTitle = "Oculus Switch";
+                    this.notifyIcon1.Icon = new Icon(@"C:\Oculus.ico");
+                    this.notifyIcon1.Visible = true;
+                    this.notifyIcon1.ShowBalloonTip(5);
+                    this.notifyIcon1.Icon = new Icon(@"C:\OculusRed2.ico");
+                }
+        }
+
+        //This opens a dialog box of how to use the App
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this,
+                " -This app must run at 'Admin' privileges to work.\n - Close any instances of this app, right click the app and choose\n 'Run This Program As An Administrator'.",
+
+                "Created by @awalkingspastic");
+        }
+
+        //This Exits the App
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+                {
+                    Application.Exit();
+
+            /*
+            DialogResult messageBoxResult = MessageBox.Show
+            ("Oculus has been turned On", "Oculus Switch", 
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (messageBoxResult == DialogResult.OK);
+
+            //MessageBox.Show("Oculus has been turned On");
+            */
+                }
+
+
+    }
+}
